7bb3bcde08011c1d1a74246e5b559f0c9c763150
Barak Levy <barak.levy@intel.com>
2017-12-25 23:09:38 +0200

    Multiple images are now shown in a list

--- app/src/main/java/levy/barak/ankihelper/CardsListAdapter.java
+++ app/src/main/java/levy/barak/ankihelper/CardsListAdapter.java
@@ -8,16 +8,19 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import levy.barak.ankihelper.utils.ImageUtils;
 
 /**
@@ -30,16 +33,16 @@ public class CardsListAdapter extends RecyclerView.Adapter<CardsListAdapter.Data
 
     public static class DataObjectHolder extends RecyclerView.ViewHolder {
         private TextView word;
-        private ImageView image;
         private TextView ipa;
         private ImageButton sound;
+        private LinearLayout imagesListLayout;
 
         public DataObjectHolder(View itemView) {
             super(itemView);
             word = itemView.findViewById(R.id.word_list_word);
-            image = itemView.findViewById(R.id.word_list_image);
             ipa = itemView.findViewById(R.id.word_list_ipa);
             sound = itemView.findViewById(R.id.word_list_sound);
+            imagesListLayout = itemView.findViewById(R.id.word_images_list);
         }
     }
 
@@ -64,14 +67,21 @@ public class CardsListAdapter extends RecyclerView.Adapter<CardsListAdapter.Data
         holder.word.setText(word.germanWord);
         holder.ipa.setText(word.ipa);
 
-        // Set image
-        Bitmap bitmap = BitmapFactory.decodeFile(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/anki_helper/" + word.imagesUrl.get(0));
-        //Bitmap bt = Bitmap.createScaledBitmap(bitmap, ImageUtils.dipToPixels(mContext, 100), ImageUtils.dipToPixels(mContext, 100), true);
-        holder.image.setImageBitmap(bitmap);
-        //bt = null;
-        bitmap = null;
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        //holder.imagesListLayout.removeAllViews();
 
-        holder.image.setOnClickListener(v -> ((TranslateActivity)mContext).zoomImageFromThumb(holder.itemView, Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/anki_helper/" + word.imagesUrl.get(0)));
+        // Set image
+        for (String imageUrl : word.imagesUrl) {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            Bitmap bitmap = decodeSampledBitmap(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/anki_helper/" + imageUrl, ImageUtils.dipToPixels(mContext, 48), ImageUtils.dipToPixels(mContext, 48));
+
+            CircleImageView imageView = (CircleImageView) inflater.inflate(R.layout.card_image_view, holder.imagesListLayout, false);
+            imageView.setImageBitmap(bitmap);
+            imageView.setOnClickListener(v -> ((TranslateActivity) mContext).zoomImageFromThumb(imageView, Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/anki_helper/" + imageUrl));
+
+            holder.imagesListLayout.addView(imageView);
+        }
 
         // Set sound
         holder.sound.setOnClickListener(v -> {
@@ -85,6 +95,44 @@ public class CardsListAdapter extends RecyclerView.Adapter<CardsListAdapter.Data
                 e.printStackTrace();
             }
         });
+    }
+
+    public static Bitmap decodeSampledBitmap(String imagePath, int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imagePath, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(imagePath, options);
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 
     @Override
